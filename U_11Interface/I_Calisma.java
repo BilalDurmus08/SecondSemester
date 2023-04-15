b426c51afdd9162ff960d60bcdb3d9e1586a305e
Bilal Durmuş <116338383+BilalDurmus08@users.noreply.github.com>
2023-04-15 22:45:38 +0300

    Delete U_11Interface directory

--- U_11Interface/I_Calisma.java
+++
@@ -1,5 +0,0 @@
-package U_11Interface;
-
-public interface I_Calisma {
-    void calisma();
-}
--- U_11Interface/I_Muhendis.java
+++
@@ -1,9 +0,0 @@
-package U_11Interface;
-
-public interface I_Muhendis {
-     void askerlikDurumu();
-     String mezuniyet_ortalamasi(double ortalama);
-     void adli_Sicil_sorgula();
-     void is_tecrubesi(String[] tecrube);
-
-}
